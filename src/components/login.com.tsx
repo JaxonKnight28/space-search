1716b9e229dcb269ce48a1640c7cbe9e4c72a360
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-01-19 08:34:51 -0700

    firebase works, testing on heroku

--- src/components/login.com.tsx
+++ src/components/login.com.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react"
 import { useNavigate } from "react-router";
 import { Button, Form } from "semantic-ui-react"
-import users from '../dataLayers/users.json';
+
+//FIREBASE---------
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyD0x7_WJenbZkGSk9Ea_vRQ-9-OK9FRSq0",
+    authDomain: "space-search-9533f.firebaseapp.com",
+    projectId: "space-search-9533f",
+    storageBucket: "space-search-9533f.appspot.com",
+    messagingSenderId: "272593411706",
+    appId: "1:272593411706:web:b0de1b50188314ca1de5c3",
+    measurementId: "G-B19H85RRQV"
+});
+
+const db = getFirestore()
+//FIREBASE----------
 
 //set types
 export type LoginValues = {
@@ -13,11 +30,17 @@ export function Login() {
     const [logInData, setLogInData] = useState<LoginValues>({})
     const navigate = useNavigate()
     let success = false;
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
+        //firebase---
+        let users: any[] = [];
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        querySnapshot.forEach((doc) => {
+            users.push(doc.data())
+        });
+        //firebase---
         success = false
         users.forEach((user) => {
             if (user.username === logInData.username && user.password === logInData.password) {
-                console.log('works');
                 success = true
                 navigate('/')
             }
--- src/components/signUp.com.tsx
+++ src/components/signUp.com.tsx
@@ -1,7 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, Form } from "semantic-ui-react";
-import users from '../dataLayers/users.json';
-//import db from './firebaseFile'
+
+//FIREBASE---------
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
+
+initializeApp({
+    apiKey: "AIzaSyD0x7_WJenbZkGSk9Ea_vRQ-9-OK9FRSq0",
+    authDomain: "space-search-9533f.firebaseapp.com",
+    projectId: "space-search-9533f",
+    storageBucket: "space-search-9533f.appspot.com",
+    messagingSenderId: "272593411706",
+    appId: "1:272593411706:web:b0de1b50188314ca1de5c3",
+    measurementId: "G-B19H85RRQV"
+});
+
+const db = getFirestore()
+//FIREBASE----------
 
 export type SignUpValues = {
     username?: string;
@@ -11,9 +27,17 @@ export type SignUpValues = {
 export function SignUpComp() {
     const [signUpData, setLogInData] = useState<SignUpValues>({})
     let success = true;
-    const handleSubmit = () => {
+
+    const handleSubmit = async () => {
+        let users: any[] = [];
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        querySnapshot.forEach((doc) => {
+            users.push(doc.data())
+        });
         success = true
         users.forEach((user) => {
+            console.log(user.username, signUpData.username);
+
             if (user.username === signUpData.username && user.password === signUpData.password) {
                 alert('That account already exists')
                 success = false
@@ -21,9 +45,15 @@ export function SignUpComp() {
         })
         if (success) {
             console.log('worked');
-            //console.log(db.collection("Users"));
-
-
+            try {
+                const docRef = await addDoc(collection(db, "Users"), {
+                    username: `${signUpData.username}`,
+                    password: `${signUpData.password}`,
+                });
+                console.log("Document written with ID: ", docRef.id);
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
         }
     }
 
